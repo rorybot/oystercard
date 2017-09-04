f6b645a0599ba69933ad48f24fc67a578ff8366f
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-09-04 14:43:56 +0100

    Can deduct balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,20 @@
 class Oystercard
   attr_reader :balance
+  MAX_BALANCE = 90
 
   def initialize(balance = 0)
     @balance = balance
   end
 
-  def top_up(new_money)
-    @balance += new_money
+  def top_up(money)
+    fail "I'm too full of money - I can only take £#{MAX_BALANCE}!" if @balance + money > MAX_BALANCE
+    @balance += money
+  end
+
+  def deduct(money)
+    #fail "I'm too full of money - I can only take £#{MAX_BALANCE}!" if
+    #@balance + money > MAX_BALANCE
+    @balance -= money
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,7 +10,19 @@ describe Oystercard do
 
   it '#top_up balance of a card' do
     oystercard.top_up(1)
-    expect(oystercard.balance).to eq 1 
+    expect(oystercard.balance).to eq 1
+  end
+
+  it '#deduct balance of a card' do
+    oystercard_with_one_pound = Oystercard.new(1)
+    oystercard_with_one_pound.deduct(1)
+    expect(oystercard_with_one_pound.balance).to eq 0
+  end
+
+  it '#top_up raise exception if balance exceeds limit' do
+    maximum_balance = Oystercard::MAX_BALANCE
+    subject.top_up maximum_balance
+    expect{subject.top_up 91 }.to raise_error "I'm too full of money - I can only take £90!"
   end
 
 end
